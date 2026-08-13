b718874b165f54ebd7e85eb0093966fe659b7e1b
parthvigirlspg-ui <parthvigirlspg@gmail.com>
2026-08-14 01:29:33 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,15 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
+}    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }    debugImplementation("androidx.compose.ui:ui-tooling")
 }
